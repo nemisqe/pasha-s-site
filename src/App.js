3358566401ff7e7358c7cd866d0c2b21c56b7eb6
nemisqe <datnemiz@gmail.com>
2020-03-26 01:33:57 +0300

    yu

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import GalleryPage from "./components/GalleryPage";
 const App = () => {
     return (
         <div className="App">
-            <HashRouter basename="/">
+            <HashRouter basename={process.env.PUBLIC_URL + '/'}>
                 <AppHeader />
                 <main role="main">
                     <Router>
-                        <Route path="/" exact component={HomePage}/>
-                        <Route path="/about" exact component={AboutPage} />
-                        <Route path="/gallery" exact component={GalleryPage} />
+                        <Route path={process.env.PUBLIC_URL + '/'} exact component={HomePage}/>
+                        <Route path={process.env.PUBLIC_URL + '/about'} exact component={AboutPage} />
+                        <Route path={process.env.PUBLIC_URL + '/gallery'} exact component={GalleryPage} />
                     </Router>
                 </main>
                 <AppFooter />
